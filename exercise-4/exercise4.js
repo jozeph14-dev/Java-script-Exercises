386cfd071a832b3959e1a3ee9ee8b821464d1515
jozeph14-dev <yuusuf6163@gmail.com>
2026-03-06 06:24:02 +0300

    exercise-4

---
+++ exercise-4/exercise4.js
@@ -0,0 +1,14 @@
+function add(a,b){
+    return(a+b)
+}
+console.log(add(22,66));
+result = add (20,60);
+console.log(result);
+
+const sub= function (x,y){
+    console.log(x+y)
+}
+sub(30,40)
+sub(70,20)
+sub(30,70)
+sub(100,20)
