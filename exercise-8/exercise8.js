152bf33e12bf28b4cb50968f1485f637addada8e
jozeph14-dev <yuusuf6163@gmail.com>
2026-03-08 17:31:03 +0300

    exercise-8

---
+++ exercise-8/exercise8.js
@@ -0,0 +1,10 @@
+let car ={
+    nameCar: "tayota corolla",
+    make: "toyota",
+    model: "corolla",
+    year: 2020
+}
+console.log(car.nameCar);
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
