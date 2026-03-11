33430cfe6e841c552406e35258a52e7d5ef28ed9
jozeph14-dev <yuusuf6163@gmail.com>
2026-03-11 22:20:52 +0300

    exercise-13

--- exercise-12/exercise12.js
+++ exercise-12/exercise12.js
@@ -4,3 +4,5 @@ let fruits = ["apple", "banana", "cherry"]
 for(let x = 0; x <fruits.length; x++){
     console.log(fruits[x])
 }
+
+
---
+++ exercise-13/exercise13.js
@@ -0,0 +1,5 @@
+let x=5;
+while(x > 0){
+    console.log(x)
+    x--
+}
